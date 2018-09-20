3d3d0bb79e3cbff1bc030e5f2bd1d62e535cb9c2
robertnowallworld <robertrahardja@roberts-mbp.lan>
2018-09-20 15:56:07 +0800

    The main get is for testing route.js and it's module insertion. /test is a post that returns the param of testVar. It's just for testing

--- route.js
+++ route.js
@@ -20,13 +20,19 @@ app.get('/', (req, res) => {
 
 
     res.write('Testing module insertion\n\n');
-    res.write('hdPath is' + hdPath + '\n\n');
-    res.write('setWeb3Provider is \n'+ setWeb3Provider + '\n\n');
+    res.write('hdPath is ' + hdPath + '\n\n');
+    res.write('setWeb3Provider is \n\n'+ setWeb3Provider + '\n\n');
     res.write('contractAddress is '+ contractAddress+ '\n\n');
     res.write('provider and rpc address is '+ provider +'\n\n');
     res.write('ABI is \n' + JSON.stringify(ABI));
     res.end();
     }
-    )
+)
+
+app.post('/test', function (req, res) {
+    var testVar = req.param('testVar');
+    res.send(testVar);
+    }   
+)
 
 app.listen(port, () => console.log(`Trial: listening on port ${port}!`))
