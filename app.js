88ff6ae707358a4c506b75c6126c52af14c555bf
robertnowallworld <robertrahardja@roberts-mbp.lan>
2018-09-26 15:07:00 +0800

    added comment at app.js to tidy things up

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//for testing connections etc
 app.use('/', require('./controller/libConnectionTest.js'));
-
 app.use('/test', require('./controller/postTest.js'))
-
 app.use('/ctest', require('./controller/ethNodeConnectionTest.js'));
+
 app.use('/balances', require('./controller/checkBalance.js'));
 app.listen(port, () => console.log(`Trial: listening on port ${port}!`))
