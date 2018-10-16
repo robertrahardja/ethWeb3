3f9029e90c36d5ae5cf3b43c5b56992be3d8ef70
robertnowallworld <robertrahardja@roberts-mbp.lan>
2018-10-16 18:09:43 +0800

    cleaned up several comments

--- test/getUserOdinEthApiTest.js
+++ test/getUserOdinEthApiTest.js
@@ -4,9 +4,6 @@ var expect = require('expect')
 var server = require('../app')
 var expect = chai.expect
 chai.use(chaiHttp)
-//var chaiAsPromised = require("chai-as-promised")
-//chai.use(chaiAsPromised)
-
 
 describe('Get user\'s Odin and Eth Balances', () => {
     it('api get should return status 200 and both Odin and Eth balance', (done) => {
@@ -15,7 +12,6 @@ describe('Get user\'s Odin and Eth Balances', () => {
             .set('content-type', 'application/x-www-form-urlencoded')
             .type('form')
             .send({ userAddress: '0xcd8e3e5b4a92cb8689da99026f11624d59b45a5c' })
-            //.send('userAddress="0xcd8e3e5b4a92cb8689da99026f11624d59b45a5c"')
             .end((err, res) => {
                 expect(res).to.have.status(200);
                 console.log("\tres status is : " + JSON.stringify(res.status));
