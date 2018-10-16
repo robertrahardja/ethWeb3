11702df58549ef994054bdcf14cf8d0a76cb7110
robertnowallworld <robertrahardja@roberts-mbp.lan>
2018-10-16 15:16:45 +0800

    Added ethPromistTest.js and getUserEthBalance.js

---
+++ test/ethPromiseTest.js
@@ -0,0 +1,25 @@
+var expect = require('expect')
+var chai = require('chai')
+var chaiAsPromised = require("chai-as-promised")
+chai.use(chaiAsPromised)
+var expect = chai.expect
+var chaiMatchPattern = require('chai-match-pattern');
+chai.use(chaiMatchPattern);
+var _ = chaiMatchPattern.getLodashModule(); 
+
+describe('Ether Promises', () => {
+  describe('Ether Balance Promise Test', () => {
+
+    it('Promise returned object with pattern {"ethBalance": Number} ', () => {
+      const getUserEthBalance = require("../controller/getUserEthBalance")
+      userAddress = '0xcd8e3e5b4a92cb8689da99026f11624d59b45a5c'
+      
+      return getUserEthBalance(userAddress).then((result) => {
+
+        expect(result).to.matchPattern(`{
+          "ethBalance": _.isNumber,
+          }`)
+      })
+    })
+  })
+})
